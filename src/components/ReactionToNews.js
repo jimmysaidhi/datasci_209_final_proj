e42baf9c8c4b76e25b6684ff9ac4af3926f3634d
bryceloomis <141188563+bryceloomis@users.noreply.github.com>
2025-07-19 16:14:31 -0400

    Update ReactionToNews.js

--- src/components/ReactionToNews.js
+++ src/components/ReactionToNews.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 function ReactionToNews() {
   return (
-    <h2>Placeholder For ReactionToNews</h2>
+    <div>
+      <h2>Reaction To News</h2>
+      <iframe 
+        src="" 
+        width="100%" 
+        height="500px"
+        frameBorder="0"
+        title="Presidential Betting Odds Chart"
+      />
+    </div>
   );
 }
 

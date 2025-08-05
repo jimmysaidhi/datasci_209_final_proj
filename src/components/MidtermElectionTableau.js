67f4f4f6497cd0129c7ea19e7f1b57c493b7d4a1
bryceloomis <141188563+bryceloomis@users.noreply.github.com>
2025-08-04 20:47:21 -0400

    Update MidtermElectionTableau.js

--- src/components/MidtermElectionTableau.js
+++ src/components/MidtermElectionTableau.js
@@ -35,7 +35,14 @@ function MidtermElectionTableau() {
       <h1 className="text-left mb-3">Electoral Map Dashboard</h1>
       <p className="text-left">
       The visualization below shows a map of the prediction-market-implied probabilities of Donald Trump winning the 2024 US Presidential Election
-      starting in April 2024. </p> 
+      starting in April 2024. This visualization is meant to offer a landing page for users, where they can see a national view of the state-level data. 
+      States are colored by the implied probability of Donald Trump winning the 2024 election, with higher probabilities in red, and lower probabilities 
+      in blue. Users can manipulate the slider on the top left to scroll through the election timeline. 
+
+      The visualization also includes more state-specific information. Users can select states of interest, by clicking on a state in the larger map.
+      In a barplot to the right of the main graph are the implied probabilities for each candidate for the selected date. Below is a line graph showing
+      prediction market probabilities for the state throughout the entirety of the election. 
+ </p> 
       <p className="text-left"> You can change the date using the slider below or get more state-specific information by clicking any of the 50 states.
       </p>
       <div ref={vizRef} />
